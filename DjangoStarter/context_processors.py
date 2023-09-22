4b34f1dc0dfc75cd616db065b065181f8acbc5e4
Nuno Bispo <nuno.m.bispo@gmail.com>
2023-09-23 00:13:12 +0200

    Site Context Processor working

--- DjangoStarter/context_processors.py
+++ DjangoStarter/context_processors.py
@@ -3,7 +3,7 @@ from decouple import config
 
 def site_settings(request):
     return {
-        'SITE_TITLE:': config('SITE_TITLE', default='DjangoStarter'),
+        'SITE_TITLE': config('SITE_TITLE', default='Django Starter'),
         'SITE_FOOTER_LINK': config('SITE_FOOTER_LINK', default='https://developer-service.io/'),
         'SITE_FOOTER_LINK_TITLE': config('SITE_FOOTER_LINK_TITLE', default='Developer Service'),
         'SITE_FOOTER_COPYRIGHT': config('SITE_FOOTER_COPYRIGHT', default='2023'),
