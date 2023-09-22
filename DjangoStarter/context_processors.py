5f08018b6d96bc08246c45f7dd65a62a07d934a5
Nuno Bispo <nuno.m.bispo@gmail.com>
2023-09-23 00:00:14 +0200

    Initial Templates

---
+++ DjangoStarter/context_processors.py
@@ -0,0 +1,11 @@
+from decouple import config
+
+
+def site_settings(request):
+    return {
+        'SITE_TITLE:': config('SITE_TITLE', default='DjangoStarter'),
+        'SITE_FOOTER_LINK': config('SITE_FOOTER_LINK', default='https://developer-service.io/'),
+        'SITE_FOOTER_LINK_TITLE': config('SITE_FOOTER_LINK_TITLE', default='Developer Service'),
+        'SITE_FOOTER_COPYRIGHT': config('SITE_FOOTER_COPYRIGHT', default='2023'),
+        'SITE_FOOTER_TWITTER_LINK': config('SITE_FOOTER_TWITTER_LINK', default='https://twitter.com/DevAsService'),
+    }
--- DjangoStarter/settings.py
+++ DjangoStarter/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account'
 ]
 
 MIDDLEWARE = [
@@ -67,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'DjangoStarter.context_processors.site_settings',
             ],
         },
     },
